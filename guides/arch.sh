8d4e3586edd56ff46922a20c6728faf9f48caf3f
Mattia46 <mattia.assogna@gmail.com>
2020-03-28 14:43:51 +0000

    updated arch guide

--- guides/arch.sh
+++ guides/arch.sh
@@ -17,14 +17,17 @@ xclip -sel clip < ~/.ssh/id_rsa.pub
 #Paste key in github
 ssh -T git@github.com
 
+# clone dotfile
+git clone git@github.com:Mattia46/dotfile.git ~
+
 # links
-ln -s dotfile/vimrc ~/.vimrc
-ln -s dotfile/zshrc ~/.zshrc
-ln -s dotfile/gitconfig ~/.gitconfig
-ln -s dotfile/gitignore ~/.gitignore
-ln -s dotfile/tmux-conf ~/.tmux-conf
-ln -s dotfile/vim ~/.vim
-ln -s dotfile/wallpaper ~/.wallpaper
-ln -s dotfile/xinitrc ~/.xinitrc
-ln -s dotfile/Xmodmap ~/.Xmodmap
-ln -s dotfile/ranger.conf ~/.config/ranger/rc.conf
+ln -s ~/dotfile/vimrc ~/.vimrc
+ln -s ~/dotfile/zshrc ~/.zshrc
+ln -s ~/dotfile/gitconfig ~/.gitconfig
+ln -s ~/dotfile/gitignore ~/.gitignore
+ln -s ~/dotfile/tmux-conf ~/.tmux-conf
+ln -s ~/dotfile/vim ~/.vim
+ln -s ~/dotfile/wallpaper ~/.wallpaper
+ln -s ~/dotfile/xinitrc ~/.xinitrc # if DMG ~/.xsession
+ln -s ~/dotfile/Xmodmap ~/.Xmodmap
+ln -s ~/dotfile/ranger.conf ~/.config/ranger/rc.conf
