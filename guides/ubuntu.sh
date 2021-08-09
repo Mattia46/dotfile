e682774ca3c0a460ba148f4b148bb96fbec175bf
mattia <mattia@deb.local>
2021-08-09 12:16:04 +0100

    updated node version and rofi run

--- guides/ubuntu.sh
+++ guides/ubuntu.sh
@@ -5,7 +5,7 @@
 sudo chown -R $(whoami) /usr/local
 
 #node & npm
-curl -sL https://deb.nodesource.com/setup_9.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
 sudo apt-get install -y nodejs
 #sudo apt-get install -y build-essential
 
